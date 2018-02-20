8b8a9602d2a55999c971d4e729f1311ba6dff3f2
pachinko <meimeihenshao@gmail.com>
2018-02-20 22:05:55 +0800

    6

---
+++ leetcode/ZigZag Conversion.go
@@ -0,0 +1,26 @@
+import "bytes"
+
+func convert(s string, numRows int) string {
+	if numRows == 1 {
+		return s
+	}
+	var sbs []bytes.Buffer = make([]bytes.Buffer, numRows, numRows)
+	x, j, isFront := 2*numRows-2, 0, true
+	for i, n := 0, len(s); i < n; i++ {
+		if (i % x) == 0 {
+			isFront = true
+		} else if (i % x) == numRows-1 {
+			isFront = false
+		}
+		sbs[j].WriteByte(s[i])
+		if isFront {
+			j += 1
+		} else {
+			j -= 1
+		}
+	}
+	for i := 1; i < numRows; i++ {
+		sbs[0].Write(sbs[i].Bytes())
+	}
+	return sbs[0].String()
+}
