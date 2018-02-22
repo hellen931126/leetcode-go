0255ff42071efcb1d71187f24fe6547f94a014ac
pachinko <meimeihenshao@gmail.com>
2018-02-22 16:44:48 +0800

    88

---
+++ leetcode/Merge Sorted Array.go
@@ -0,0 +1,22 @@
+/*
+Given two sorted integer arrays nums1 and nums2, merge nums2 into nums1 as one sorted array.
+*/
+
+func merge(nums1 []int, m int, nums2 []int, n int) {
+	i := m - 1
+	j := n - 1
+
+	for k := m + n - 1; i >= 0 && j >= 0 && k >= 0; k-- {
+		if nums2[j] >= nums1[i] {
+			nums1[k] = nums2[j]
+			j--
+			continue
+		}
+		nums1[k] = nums1[i]
+		i--
+	}
+	for ; j >= 0; j-- {
+		nums1[j] = nums2[j]
+	}
+	return
+}
