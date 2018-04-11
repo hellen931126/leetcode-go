e10b22f67408cc302116b5071e9c15f1af969489
pachinko <meimeihenshao@gmail.com>
2018-04-11 13:02:40 +0800

    083

---
+++ 083 Remove Duplicates from Sorted List.go
@@ -0,0 +1,23 @@
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+
+func deleteDuplicates(head *ListNode) *ListNode {
+	if head == nil {
+		return nil
+	}
+
+	temp := head
+	for temp.Next != nil {
+		if temp.Val == temp.Next.Val {
+			temp.Next = temp.Next.Next
+			continue
+		}
+		temp = temp.Next
+	}
+	return head
+}
